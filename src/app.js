3b3413f49b745e1ca09d324c437a4203a79aee25
Rishh2106 <rishijaon05@gmail.com>
2025-03-17 01:11:26 +0530

    render test push

--- src/app.js
+++ src/app.js
@@ -1,19 +1,45 @@
-let express = require( 'express' );
-let app = express();
-let server = require( 'http' ).Server( app );
-let io = require( 'socket.io' )( server );
-let stream = require( './ws/stream' );
-let path = require( 'path' );
-let favicon = require( 'serve-favicon' );
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const stream = require('./ws/stream');
+const path = require('path');
+const favicon = require('serve-favicon');
+const cors = require('cors');
+const compression = require('compression');
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+// Production optimizations
+app.use(compression()); // Compress all responses
+app.use(cors()); // Enable CORS for all requests
 
-app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
-} );
+// Security headers
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    next();
+});
 
+// Static files
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-io.of( '/stream' ).on( 'connection', stream );
+// Main route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-server.listen( 3000 );
+// Socket.IO configuration
+io.of('/stream').on('connection', stream);
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
